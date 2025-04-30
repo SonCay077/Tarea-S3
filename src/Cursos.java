abe5fd67f9561ca0dcdb74e5ee8c7ec8a1451a3f
HP 22c014la <carlos.serrano03@epn.edu.ec>
2025-04-30 18:32:31 -0500

    Tarea 3

---
+++ src/Cursos.java
@@ -0,0 +1,57 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+class Curso{
+    String nombre, codigo;
+    int creditos;
+
+    public Curso(String nombre, String codigo, int creditos){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.creditos = creditos;
+    }
+    public void mostrarInfo(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Creditos: " + creditos);
+    }
+    public void editarCredito(Scanner entrada){
+        System.out.println("Actualizando créditos...");
+        System.out.println("Actualizar créditos del curso: ");
+        creditos = entrada.nextInt();
+        entrada.nextLine();
+    }
+}
+public class Cursos{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--Ingreso de cursos--");
+        System.out.println("Ingrese el nombre del curso: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese el codigo del curso: ");
+        String codigo = scanner.nextLine();
+        System.out.println("Ingrese los creditos del curso: ");
+        int creditos = scanner.nextInt();
+        scanner.nextLine();
+        Curso curso = new Curso(nombre, codigo, creditos);
+
+        curso.mostrarInfo();
+
+        System.out.println("Editar créditos del curso (SI/NO): ");
+        String selec = scanner.nextLine();
+        selec = selec.toUpperCase(Locale.ROOT);
+
+        while (selec.equals("SI")){
+            System.out.println("Ingrese el nuevo valor de créditos: ");
+            curso.editarCredito(scanner);
+            System.out.println("------------------");
+            System.out.println("Valor actualizado.");
+            System.out.println("------------------");
+            curso.mostrarInfo();
+        }
+        System.out.println("Curso ingresado.");
+        scanner.close();
+    }
+}
